cca22082dc820c19378a47d3ed79c3b215500569
jspencerhopkins <jspencerhopkins@gmail.com>
2015-02-21 10:16:00 -0500

    Add Game resource

---
+++ db/migrate/20150221151457_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration
+  def change
+    create_table :games do |t|
+      t.string :home_team
+      t.string :away_team
+      t.integer :home_score
+      t.integer :away_score
+
+      t.timestamps null: false
+    end
+  end
+end
